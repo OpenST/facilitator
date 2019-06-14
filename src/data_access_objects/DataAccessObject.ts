19cb5e1540f36cc9c283d58dad1f09bc4bb69291
Paruyr Gevorgyan <paruyr@ost.com>
2019-06-14 10:43:58 +0200

    Implements draft version of stake request model
    See: #53

---
+++ src/data_access_objects/DataAccessObject.ts
@@ -0,0 +1,121 @@
+// Copyright 2019 OpenST Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// ----------------------------------------------------------------------------
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { Database } from 'sqlite3';
+
+import sqlite3 = require('sqlite3');
+import assert = require('assert');
+
+export default class DataAccessObject {
+  /* Storage */
+
+  private db: Database;
+
+
+  /* Public Functions */
+
+  public static createFromFile(dbFilePath: string): DataAccessObject {
+    assert.notStrictEqual(dbFilePath, '');
+
+    const db = new sqlite3.Database(
+      dbFilePath,
+      // eslint-disable-next-line no-bitwise
+      sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+      (err: Error): void => {
+        if (err !== null) {
+          throw err;
+        }
+      },
+    );
+    return new DataAccessObject(db);
+  }
+
+  public static createInMemory(): DataAccessObject {
+    const db = new sqlite3.Database(
+      ':memory:',
+      // eslint-disable-next-line no-bitwise
+      sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+      (err: Error): void => {
+        if (err !== null) {
+          throw err;
+        }
+      },
+    );
+
+    return new DataAccessObject(db);
+  }
+
+  public async run(sql: string, params?: any): Promise<void> {
+    return new Promise(
+      (resolve, reject): Database => this.db.run(
+        sql, (params === undefined ? [] : params), (err: Error | null): void => {
+          if (err !== null) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        },
+      ),
+    );
+  }
+
+  public async get(sql: string, params?: any): Promise<any> {
+    return new Promise(
+      (resolve, reject): Database => this.db.get(
+        sql, (params === undefined ? [] : params), (err: Error | null, row?: any): void => {
+          if (err !== null) {
+            reject(err);
+          } else {
+            resolve(row);
+          }
+        },
+      ),
+    );
+  }
+
+  public async all(sql: string, params?: any): Promise<any> {
+    return new Promise(
+      (resolve, reject): Database => this.db.all(
+        sql, (params === undefined ? [] : params), (err: Error | null, rows?: any): void => {
+          if (err !== null) {
+            reject(err);
+          } else {
+            resolve(rows);
+          }
+        },
+      ),
+    );
+  }
+
+  public close(): void {
+    this.db.close(
+      (err: Error): void => {
+        if (err !== null) {
+          throw err;
+        }
+      },
+    );
+  }
+
+
+  /* Private Functions */
+
+  private constructor(_database: Database) {
+    this.db = _database;
+  }
+}
---
+++ src/data_access_objects/StakeRequestRepository.ts
@@ -0,0 +1,138 @@
+// Copyright 2019 OpenST Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// ----------------------------------------------------------------------------
+
+import DataAccessObject from './DataAccessObject';
+
+export interface StakeRequest {
+  stakeRequestHash: string;
+  messageHash: string;
+  amount: number;
+  beneficiary: string;
+  gasPrice: number;
+  gasLimit: number;
+  nonce: number;
+  gateway: string;
+  stakerProxy: string;
+}
+
+export class StakeRequestRepository {
+  private dao: DataAccessObject;
+
+  public static readonly tableName: string = 'stake_request';
+
+  public static readonly stakeRequestHashColumnName: string = 'stake_request_hash';
+
+  public static readonly messageHashColumnName: string = 'message_hash';
+
+  public static readonly amountColumnName: string = 'amount';
+
+  public static readonly beneficiaryColumnName: string = 'beneficiary';
+
+  public static readonly gasPriceColumnName: string = 'gas_price';
+
+  public static readonly gasLimitColumnName: string = 'gas_limit';
+
+  public static readonly nonceColumnName: string = 'nonce';
+
+  public static readonly gatewayColumnName: string = 'gateway';
+
+  public static readonly stakerProxyColumnName: string = 'staker_proxy';
+
+
+  /* Public Functions */
+
+  public constructor(_dao: DataAccessObject) {
+    this.dao = _dao;
+  }
+
+  public async createTable(): Promise<void> {
+    const createTableStmt = `CREATE TABLE [IF NOT EXISTS] ${StakeRequestRepository.tableName} `
+      + `${StakeRequestRepository.stakeRequestHashColumnName} TEXT PRIMARY KEY `
+      + '( '
+      + `${StakeRequestRepository.messageHashColumnName} TEXT, `
+      + `${StakeRequestRepository.amountColumnName} INTEGER, `
+      + `${StakeRequestRepository.beneficiaryColumnName} TEXT, `
+      + `${StakeRequestRepository.gasPriceColumnName} INTEGER, `
+      + `${StakeRequestRepository.gasLimitColumnName} INTEGER, `
+      + `${StakeRequestRepository.nonceColumnName} INTEGER, `
+      + `${StakeRequestRepository.gatewayColumnName} TEXT, `
+      + `${StakeRequestRepository.stakerProxyColumnName} TEXT `
+      + '); ';
+
+    return this.dao.run(createTableStmt);
+  }
+
+  public async create(stakeRequest: StakeRequest): Promise<void> {
+    const insertStmt = `INSERT INTO ${StakeRequestRepository.tableName} `
+      + '( '
+      + `${StakeRequestRepository.stakeRequestHashColumnName}, `
+      + `${StakeRequestRepository.messageHashColumnName}, `
+      + `${StakeRequestRepository.amountColumnName}, `
+      + `${StakeRequestRepository.beneficiaryColumnName}, `
+      + `${StakeRequestRepository.gasPriceColumnName}, `
+      + `${StakeRequestRepository.gasLimitColumnName}, `
+      + `${StakeRequestRepository.nonceColumnName}, `
+      + `${StakeRequestRepository.gatewayColumnName}, `
+      + `${StakeRequestRepository.stakerProxyColumnName} `
+      + ') '
+      + 'VALUES '
+      + '( '
+      + `${stakeRequest.stakeRequestHash}, `
+      + `${stakeRequest.messageHash}, `
+      + `${stakeRequest.amount}, `
+      + `${stakeRequest.beneficiary}, `
+      + `${stakeRequest.gasPrice}, `
+      + `${stakeRequest.gasLimit}, `
+      + `${stakeRequest.nonce}, `
+      + `${stakeRequest.gateway}, `
+      + `${stakeRequest.stakerProxy} `
+      + '); ';
+
+    return this.dao.run(insertStmt);
+  }
+
+  public async get(stakeRequestHash: string): Promise<StakeRequest | undefined> {
+    const getStmt = 'SELECT '
+      + `${StakeRequestRepository.stakeRequestHashColumnName}, `
+      + `${StakeRequestRepository.messageHashColumnName}, `
+      + `${StakeRequestRepository.amountColumnName}, `
+      + `${StakeRequestRepository.beneficiaryColumnName}, `
+      + `${StakeRequestRepository.gasPriceColumnName}, `
+      + `${StakeRequestRepository.gasLimitColumnName}, `
+      + `${StakeRequestRepository.nonceColumnName}, `
+      + `${StakeRequestRepository.gatewayColumnName}, `
+      + `${StakeRequestRepository.stakerProxyColumnName} `
+      + `FROM ${StakeRequestRepository.tableName} `
+      + `WHERE ${StakeRequestRepository.stakeRequestHashColumnName} = '${stakeRequestHash}'; `;
+
+    const raw = await this.dao.get(getStmt);
+    if (raw === undefined) {
+      return raw;
+    }
+
+    return {
+      stakeRequestHash: raw[StakeRequestRepository.stakeRequestHashColumnName],
+      messageHash: raw[StakeRequestRepository.messageHashColumnName],
+      amount: raw[StakeRequestRepository.amountColumnName],
+      beneficiary: raw[StakeRequestRepository.beneficiaryColumnName],
+      gasPrice: raw[StakeRequestRepository.gasPriceColumnName],
+      gasLimit: raw[StakeRequestRepository.gasLimitColumnName],
+      nonce: raw[StakeRequestRepository.nonceColumnName],
+      gateway: raw[StakeRequestRepository.gatewayColumnName],
+      stakerProxy: raw[StakeRequestRepository.stakerProxyColumnName],
+    };
+  }
+}
