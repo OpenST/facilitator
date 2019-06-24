94c2ec4bf67e84a4401b71c3d09938d62a609092
abhayks1 <abhay@ost.com>
2019-06-24 18:52:47 +0530

    Rename facilitator to Facilitator

--- test/facilitator.sh
+++ test/facilitator.sh
@@ -3,7 +3,7 @@
 auxChainId="301"
 homedir=$HOME
 facilitatorConfigPath="${homedir}/.mosaic/${auxChainId}/facilitator-config.json"
-mosaicConfigPath="./test/facilitator/testdata/mosaic-config.json"
+mosaicConfigPath="./test/Facilitator/testdata/mosaic-config.json"
 
 # Prints an error string to stdout.
 function error {
