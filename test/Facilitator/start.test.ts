e64450d798ec3dadc7a0278a934b64c341c2e136
abhayks1 <abhay@ost.com>
2019-06-26 12:38:55 +0530

    Better variables naming

--- test/Facilitator/start.test.ts
+++ test/Facilitator/start.test.ts
@@ -7,14 +7,14 @@ import { Config } from '../../src/Config';
 import SpyAssert from '../utils/SpyAssert';
 
 describe('Facilitator.start()', () => {
-  it('should work with correct parameters', async () => {
+  it('should start facilitation', async () => {
     const mockGraphClient = sinon.createStubInstance(GraphClient);
     const graphClientSpy = sinon.replace(
       GraphClient,
       'getClient',
       sinon.fake.returns(mockGraphClient),
     );
-    const subscriberStub = sinon.stub(Subscriber.prototype, 'subscribe');
+    const subscribeStub = sinon.stub(Subscriber.prototype, 'subscribe');
 
     const subGraphDetails = Facilitator.getSubscriptionDetails();
     const configStub = sinon.createStubInstance(Config);
@@ -31,12 +31,12 @@ describe('Facilitator.start()', () => {
     );
 
     SpyAssert.assert(
-      subscriberStub,
+      subscribeStub,
       2,
       [[], []],
     );
 
-    subscriberStub.restore();
+    subscribeStub.restore();
     sinon.restore();
   });
 });
--- test/Facilitator/stop.test.ts
+++ test/Facilitator/stop.test.ts
@@ -7,30 +7,30 @@ import { Config } from '../../src/Config';
 import SpyAssert from '../utils/SpyAssert';
 
 describe('Facilitator.stop()', () => {
-  it('stop should work properly', async () => {
+  it('should stop facilitation', async () => {
     const mockGraphClient = sinon.createStubInstance(GraphClient);
     sinon.replace(
       GraphClient,
       'getClient',
       sinon.fake.returns(mockGraphClient),
     );
-    const subscriberStub = sinon.stub(Subscriber.prototype, 'subscribe');
+    const subscribeStub = sinon.stub(Subscriber.prototype, 'subscribe');
 
     const configStub = sinon.createStubInstance(Config);
     const facilitator = new Facilitator(configStub);
     await facilitator.start();
 
-    const unSubscriberStub = sinon.stub(Subscriber.prototype, 'unsubscribe');
+    const unSubscribeStub = sinon.stub(Subscriber.prototype, 'unsubscribe');
     await facilitator.stop();
 
     SpyAssert.assert(
-      unSubscriberStub,
+      unSubscribeStub,
       2,
       [[], []],
     );
 
-    subscriberStub.restore();
-    unSubscriberStub.restore();
+    subscribeStub.restore();
+    unSubscribeStub.restore();
     sinon.restore();
   });
 });
