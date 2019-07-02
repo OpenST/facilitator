3f3c91e762404d6a185334a25273c5431d1fb7a8
Puneet Khushwani <puneet.khushwani@ost.com>
2019-07-02 12:02:41 +0530

    added spy assert calls to test cases

--- test/Account/unlock.test.ts
+++ test/Account/unlock.test.ts
@@ -1,10 +1,10 @@
-
+import * as sinon from 'sinon';
 import { assert } from 'chai';
 
 import Account from '../../src/Account';
+import SpyAssert from '../utils/SpyAssert';
 
 const Web3 = require('web3');
-
 
 describe('Unlock', () => {
   let web3: any;
@@ -16,14 +16,38 @@ describe('Unlock', () => {
     web3 = new Web3();
     validPassword = 'validPassword';
     accountCreationResponse = Account.create(web3, validPassword);
-    accountObject = new Account(accountCreationResponse.address, accountCreationResponse.encryptedKeyStore);
+    accountObject = new Account(
+      accountCreationResponse.address,
+      accountCreationResponse.encryptedKeyStore
+    );
   });
 
   it('should unlock successfully with valid password', () => {
+    const dummyAccount = web3.eth.accounts.create();
+    sinon.replace(
+      web3.eth.accounts,
+      'decrypt',
+      sinon.fake.returns(dummyAccount),
+    );
+    sinon.replace(
+      web3.eth.accounts.wallet,
+      'add',
+      sinon.fake.returns(dummyAccount),
+    );
     assert.strictEqual(
       accountObject.unlock(web3, validPassword),
       true,
       'should return true for successful unlock',
+    );
+    SpyAssert.assert(
+      web3.eth.accounts.decrypt,
+      1,
+      [[accountObject.encryptedKeyStore, validPassword]],
+    );
+    SpyAssert.assert(
+      web3.eth.accounts.wallet.add,
+      1,
+      [[dummyAccount]],
     );
   });
 
