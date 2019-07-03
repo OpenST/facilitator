281ea9b545175f4b0438c3f3980ca9ed6445f191
Sarvesh Jain <sarvesh@ost.com>
2019-07-03 13:10:00 +0530

    Added anchor handler

--- src/handlers/AnchorHandler.ts
+++ src/handlers/AnchorHandler.ts
@@ -7,17 +7,30 @@ import {
 import Logger from '../Logger';
 import { AuxiliaryChainRecordNotFoundException } from '../Exception';
 
+/**
+ * This class handles Anchor event
+ */
 export default class AnchorHandler extends ContractEntityHandler<AuxiliaryChain> {
   private auxiliaryChainRepository: AuxiliaryChainRepository;
 
   private auxiliaryChainID: number;
 
+  /**
+   * Constructor
+   * @param auxiliaryChainRepository Instance of auxiliary chain repository.
+   * @param auxiliaryChainID Auxiliary chain Id.
+   */
   public constructor(auxiliaryChainRepository: AuxiliaryChainRepository, auxiliaryChainID: number) {
     super();
     this.auxiliaryChainRepository = auxiliaryChainRepository;
     this.auxiliaryChainID = auxiliaryChainID;
   }
 
+  /**
+   * This method update latest origin block height.
+   *
+   * @param transactions Bulk transactions.
+   */
   public async persist(transactions: any[]): Promise<AuxiliaryChain[]> {
     const chainRecord = await this.auxiliaryChainRepository.get(this.auxiliaryChainID);
     if (chainRecord == null) {
