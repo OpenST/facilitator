987d3e5c740c6d781f047b640cb2b4e9dca8bb6a
abhayks1 <abhay@ost.com>
2019-07-11 13:55:29 +0530

    ContractEntity alignment

--- src/models/ContractEntity.ts
+++ src/models/ContractEntity.ts
@@ -9,7 +9,7 @@ export default class ContractEntity extends Comparable<ContractEntity> {
 
   public entityType: string;
 
-  public timestamp: BigNumber;
+  public timestamp?: BigNumber;
 
   public createdAt?: Date;
 
@@ -26,7 +26,7 @@ export default class ContractEntity extends Comparable<ContractEntity> {
   public constructor(
     contractAddress: string,
     entityType: string,
-    timestamp: BigNumber,
+    timestamp?: BigNumber,
     createdAt?: Date,
     updatedAt?: Date,
   ) {
