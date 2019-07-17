a888c0846746d777bd0ac653f422cbbe6813a775
Gulshan Vasnani <gulshan@ost.com>
2019-07-17 12:30:26 +0530

    Added unit test case for isValidSecret method of Message model

---
+++ test/models/Message/isValidSecret.test.ts
@@ -0,0 +1,37 @@
+import Message from '../../../src/models/Message';
+import assert from '../../test_utils/assert';
+
+const web3utils = require('web3-utils');
+
+describe('AuxiliaryChain::get', (): void => {
+  const message = new Message(
+    '0x000000000000000000000000000000000000000000000000000001',
+  );
+
+  message.secret = '1';
+
+  it('should return true when hashlock is generated from correct secret', async (): Promise<void> => {
+    const generatedHashLock = web3utils.keccak256(message.secret).toString();
+    message.hashLock = generatedHashLock;
+    const result = message.isValidSecret();
+
+    assert.strictEqual(
+      result,
+      true,
+      'Hashlock generated from secret should be same',
+    );
+  });
+
+  it('should return null when querying for non-existing '
+    + 'chainId', async (): Promise<void> => {
+    message.hashLock = '12345';
+
+    const result = message.isValidSecret();
+
+    assert.strictEqual(
+      result,
+      false,
+      'Hashlock generated from secret should be different',
+    );
+  });
+});
