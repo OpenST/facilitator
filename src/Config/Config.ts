3b42aceaa5912ea05cd99a1f5f86c33c5d02104a
Puneet Khushwani <puneet.khushwani@ost.com>
2019-07-18 12:57:47 +0530

    bug fix to fetch encryptedAccounts for worker

--- src/Config/Config.ts
+++ src/Config/Config.ts
@@ -274,7 +274,7 @@ export class Config {
     if (!chain.password) {
       throw new WorkerPasswordNotFoundException(`password not found for ${chain.worker}`);
     }
-    const account = new Account(chain.worker, this.facilitator.encryptedAccounts[chain.rpc]);
+    const account = new Account(chain.worker, this.facilitator.encryptedAccounts[chain.worker]);
     const web3 = new Web3(chain.rpc);
     account.unlock(web3, chain.password);
     return web3;
