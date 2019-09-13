9284ae39fc55d8387e13757c7f4a68f0b9bf7841
Gulshan Vasnani <gulshan@ost.com>
2019-09-13 12:21:46 +0530

    Reverted gasprice in constants

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,3 +1,3 @@
-export const ORIGIN_GAS_PRICE = '100000000000'; // 10 Gwei
-export const AUXILIARY_GAS_PRICE = '0x174876E800'; // 1 Gwei
+export const ORIGIN_GAS_PRICE = '0x2540BE400'; // 10 Gwei
+export const AUXILIARY_GAS_PRICE = '0x3B9ACA00'; // 1 Gwei
 export const MESSAGE_BOX_OFFSET = '7';
