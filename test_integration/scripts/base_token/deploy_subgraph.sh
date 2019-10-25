7128f79436d9f4e101263ee386a7b50dc01b00e5
Puneet Khushwani <puneet.khushwani@ost.com>
2019-10-25 09:59:25 +0530

    graph url's for sub-graph deployments to come from ENV vars

--- test_integration/scripts/base_token/deploy_subgraph.sh
+++ test_integration/scripts/base_token/deploy_subgraph.sh
@@ -3,11 +3,15 @@
 originChain="dev-origin"
 auxiliaryChain="dev-auxiliary"
 auxChainIdentifier="1000"
+originGraphAdminRPC="http://localhost:9535"
+originGraphIPFS="http://localhost:6516"
+auxiliaryGraphAdminRPC="http://localhost:9020"
+auxiliaryGraphIPFS="http://localhost:6001"
 
 echo deploying baseToken sub-graph for $originChain chain
-./node_modules/.bin/mosaic subgraph $originChain $auxChainIdentifier origin $ORIGIN_GRAPH_ADMIN_RPC $ORIGIN_GRAPH_IPFS
+./node_modules/.bin/mosaic subgraph $originChain $auxChainIdentifier origin $originGraphAdminRPC $originGraphIPFS
 sleep 10
 
 echo deploying baseToken sub-graph for $auxiliaryChain chain
-./node_modules/.bin/mosaic subgraph $originChain $auxChainIdentifier auxiliary $AUXILIARY_GRAPH_ADMIN_RPC $AUXILIARY_GRAPH_IPFS
+./node_modules/.bin/mosaic subgraph $originChain $auxChainIdentifier auxiliary $auxiliaryGraphAdminRPC $auxiliaryGraphIPFS
 sleep 10
