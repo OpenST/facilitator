f69092cdfc3b55f2ee6dc478e1a84ed911603276
bruce9steve <shahrukh@ost.com>
2020-02-19 12:37:06 +0530

    Implement WithdrawIntent model and WithdrawIntent repository.

---
+++ src/m1_facilitator/models/WithdrawIntent.ts
@@ -0,0 +1,93 @@
+// Copyright 2020 OpenST Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// ----------------------------------------------------------------------------
+
+import BigNumber from 'bignumber.js';
+
+import Comparable from '../../observer/Comparable';
+
+/**
+ * Represents WithdrawIntent model object.
+ */
+
+export default class WithdrawIntent extends Comparable<WithdrawIntent> {
+  public intentHash: string;
+
+  public messageHash: string;
+
+  public tokenAddress?: string;
+
+  public amount?: BigNumber;
+
+  public beneficiary?: string;
+
+  public createdAt?: Date;
+
+  public updatedAt?: Date;
+
+  /**
+   * Constructor to set fields of WithdrawIntent model.
+   *
+   * @param intentHash Withdraw intent hash.
+   * @param messageHash Message hash.
+   * @param tokenAddress Value token address.
+   * @param amount Withdraw amount.
+   * @param beneficiary Beneficiary address.
+   * @param createdAt Time at which record is created.
+   * @param updatedAt Time at which record is updated.
+   */
+
+  public constructor(
+    intentHash: string,
+    messageHash: string,
+    tokenAddress?: string,
+    amount?: BigNumber,
+    beneficiary?: string,
+    createdAt?: Date,
+    updatedAt?: Date,
+  ) {
+    super();
+    this.intentHash = intentHash;
+    this.messageHash = messageHash;
+    this.tokenAddress = tokenAddress;
+    this.amount = amount;
+    this.beneficiary = beneficiary;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  /**
+   * Compares two withdraw intent models.
+   *
+   * @param other A withdraw intent object to compare with.
+   *
+   * @returns 0 if two objects are equal, 1 if the current object is greater
+   *                 and -1 if the specified object is greater.
+   */
+  public compareTo(other: WithdrawIntent): number {
+    const currentKey = this.intentHash;
+    const specifiedKey = other.intentHash;
+
+    if (currentKey > specifiedKey) {
+      return 1;
+    }
+
+    if (currentKey < specifiedKey) {
+      return -1;
+    }
+
+    return 0;
+  }
+}
---
+++ test/m1_facilitator/repositories/WithdrawIntentRepository/util.ts
@@ -0,0 +1,74 @@
+// Copyright 2020 OpenST Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// ----------------------------------------------------------------------------
+
+
+import assert from '../../../test_utils/assert';
+import WithdrawIntent from '../../../../src/m1_facilitator/models/WithdrawIntent';
+
+const Util = {
+  assertWithdrawIntentAttributes(
+    inputWithdrawIntent: WithdrawIntent,
+    expectedWithdrawIntent: WithdrawIntent,
+  ): void {
+    assert.strictEqual(
+      inputWithdrawIntent.intentHash,
+      expectedWithdrawIntent.intentHash,
+      'Mismatch in intent hash.',
+    );
+
+    assert.strictEqual(
+      inputWithdrawIntent.messageHash,
+      expectedWithdrawIntent.messageHash,
+      'Mismatch in message hash.',
+    );
+
+    assert.strictEqual(
+      inputWithdrawIntent.tokenAddress,
+      expectedWithdrawIntent.tokenAddress,
+      'Mismatch in token address.',
+    );
+
+    if (inputWithdrawIntent.amount && expectedWithdrawIntent.amount) {
+      assert.isOk(
+        inputWithdrawIntent.amount.eq(expectedWithdrawIntent.amount),
+        'Mismatch in Withdraw amount.',
+      );
+    }
+
+    
+    assert.strictEqual(
+      inputWithdrawIntent.beneficiary,
+      expectedWithdrawIntent.beneficiary,
+      'Mismatch in beneficiary address.',
+    );
+
+    if (inputWithdrawIntent.createdAt && expectedWithdrawIntent.createdAt) {
+      assert.strictEqual(
+        inputWithdrawIntent.createdAt.getTime(),
+        expectedWithdrawIntent.createdAt.getTime(),
+        'Expected created at time is different than the one received in response.',
+      );
+    }
+
+    assert.isNotNull(
+      inputWithdrawIntent.updatedAt,
+      'Updated at should not be null',
+    );
+  },
+
+};
+
+export default Util;
