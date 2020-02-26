1f0804394a36055eeacbe038f36bc2fd36fd31bc
abhayks1 <abhay@ost.com>
2020-02-26 15:59:17 +0530

    refactored AnchorMapping

--- subgraph/auxiliary/src/AnchorMapping.ts
+++ subgraph/auxiliary/src/AnchorMapping.ts
@@ -2,17 +2,10 @@ import { StateRootAvailable } from '../generated/MosaicFacilitator/Anchor';
 import { AvailableStateRoot } from '../generated/schema';
 
 export default function handleStateRootAvailable(event: StateRootAvailable): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = AvailableStateRoot.load(event.transaction.from.toHex());
+  const entity = new AvailableStateRoot(
+    `${event.transaction.hash.toHex()}-${event.logIndex.toString()}`,
+  );
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new AvailableStateRoot(event.transaction.from.toHex());
-  }
-
-  // Entity fields can be set based on event parameters
   entity.blockHeight = event.params._blockNumber;
   entity.stateRoot = event.params._stateRoot;
   entity.blockNumber = event.block.number;
@@ -20,6 +13,5 @@ export default function handleStateRootAvailable(event: StateRootAvailable): voi
   entity.contractAddress = event.address;
   entity.uts = event.block.timestamp;
 
-  // Entities can be written to the store with `.save()`
   entity.save();
 }
