f83d91f5a4f1e98f4193963c00c574d4b96f8c81
Paruyr Gevorgyan <paruyr@ost.com>
2020-03-04 11:51:21 +0100

    Updates file header

--- src/common/Exception.ts
+++ src/common/Exception.ts
@@ -1,4 +1,4 @@
-// Copyright 2019 OpenST Ltd.
+// Copyright 2020 OpenST Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,8 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-// ----------------------------------------------------------------------------
 
 /* eslint-disable import/prefer-default-export */
 
