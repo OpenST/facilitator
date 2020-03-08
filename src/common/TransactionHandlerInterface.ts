151f406765c70cfce4b9fd0addfd874ed419e9ce
Paruyr Gevorgyan <paruyr@ost.com>
2020-03-08 18:42:10 +0100

    Improves TransactionHandlerInterface documentation

--- src/common/TransactionHandlerInterface.ts
+++ src/common/TransactionHandlerInterface.ts
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/** Defines an interface for transaction handler classes. */
 export default interface TransactionHandlerInterface {
+
+  /**
+   * The handle() function accepts transactions of different kinds
+   * and appropriately handles them.
+   */
   handle(bulkTransactions: any): Promise<void>;
 }
