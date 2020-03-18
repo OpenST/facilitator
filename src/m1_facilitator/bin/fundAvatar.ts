605fa540c597110c9c9d88335e822512eda389ab
Vindeep07 <vindeep@ost.com>
2020-03-18 14:57:22 +0530

    Initial Implementation to fund Avatar

---
+++ src/m1_facilitator/bin/fundAvatar.ts
@@ -0,0 +1,32 @@
+// Copyright 2020 OpenST Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import fundAvatar from 'commander';
+import FundAvatar from '../commands/FundAvatar';
+import Logger from '../../common/Logger';
+
+fundAvatar.option('-m, --manifest <manifest>', 'Path to manifest file')
+  .action(
+    async (
+      options: {
+        manifest: string;
+      }): Promise<void> => {
+      try {
+        await new FundAvatar(options.manifest).execute();
+      } catch (e) {
+        Logger.error(`Error in fundAvatar command. Reason: ${e.message}`);
+        process.exit(1);
+      }
+    },
+  ).parse(process.argv);
---
+++ src/m1_facilitator/commands/FundAvatar.ts
@@ -0,0 +1,65 @@
+// Copyright 2020 OpenST Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Web3 from 'web3';
+import Command from './Command';
+import Manifest from '../manifest/Manifest';
+
+const BN = require('bn.js');
+const axios = require('axios');
+
+const THRESHOLD = new BN(100);
+
+async function fundFromFaucet(beneficiary: string, chain: string): Promise<void> {
+  console.log(`Funding ${beneficiary} for chain ${chain}`);
+  const response = await axios.post(
+    'https://faucet.mosaicdao.org',
+    {
+      beneficiary: `${beneficiary}@${chain}`,
+    },
+  );
+  console.log(`Transaction hash is ${response.data.txHash}`);
+}
+
+async function checkBalance(account: string, web3: Web3): Promise<void> {
+  const avatarBalance = await web3.eth.getBalance(account);
+  console.log('Avatar Balance ==>', avatarBalance.toString());
+  if (new BN(avatarBalance) < THRESHOLD) {
+    fundFromFaucet(account, '1405');
+  }
+}
+
+export default class FundAvatar implements Command {
+  private manifestPath: string;
+
+  /**
+   * Construct FundAvatar instance with params.
+   *
+   * @param manifestPath Path of manifest file.
+   */
+  public constructor(manifestPath: string) {
+    this.manifestPath = manifestPath;
+  }
+
+  /**
+   * Executes fundAvatar command
+   */
+  public async execute(): Promise<void> {
+    const manifest = Manifest.fromFile(this.manifestPath);
+    Object.keys(manifest.avatarAccounts).forEach((address: string): void => {
+      const acc = manifest.avatarAccounts[address];
+      checkBalance(acc.address, manifest.metachain.auxiliaryChain.web3);
+    });
+  }
+}
